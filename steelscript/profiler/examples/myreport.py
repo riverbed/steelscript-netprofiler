4b83e60c34dc68599b0a298cf269fe4e39fe4918
Mike Garabedian <mike.garabedian@riverbed.com>
2014-04-17 15:29:31 -0400

    Refactor package directories

---
+++ steelscript/profiler/examples/myreport.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the 
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").  
+# This software is distributed "AS IS" as set forth in the License.
+
+
+import sys
+import pprint
+
+import steelscript
+import steelscript.profiler
+
+from steelscript.common.service import UserAuth
+from steelscript.profiler.filters import TimeFilter
+from steelscript.profiler.report import TrafficSummaryReport
+
+# connection information
+username = '<username>'
+password = '<password>'
+host = '<profiler.ip.address>'
+
+if (username == '<username>' or
+        password == '<password>' or
+        host == '<profiler.ip.address>'):
+    print "Update the username, password, and profiler host values before running this script."
+    sys.exit(0)
+
+auth = UserAuth(username, password)
+
+p = steelscript.profiler.Profiler(host, auth=auth)
+report = TrafficSummaryReport(p)
+
+columns = [p.columns.key.host_ip,
+           p.columns.value.avg_bytes,
+           p.columns.value.network_rtt]
+sort_column = p.columns.value.avg_bytes
+timefilter = TimeFilter.parse_range("last 15 m")
+
+report.run('hos', columns, timefilter=timefilter, sort_col=sort_column)
+data = report.get_data()
+legend = report.get_legend()
+report.delete()
+
+pprint.pprint(data[:10])
+
+
---
+++ steelscript/profiler/examples/top_ports.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the 
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").  
+# This software is distributed "AS IS" as set forth in the License.
+
+
+
+from steelscript.profiler import *
+from steelscript.profiler.app import ProfilerApp
+from steelscript.profiler.filters import TimeFilter, TrafficFilter
+
+import pprint
+
+def main(app):
+    # Create and run a traffic summary report of all server ports in use
+    # by hosts in 10/8
+    report = TrafficSummaryReport(app.profiler)
+
+    # Run the report
+    report.run(
+        groupby = app.profiler.groupbys.port,
+        columns = [app.profiler.columns.key.protoport,
+                   app.profiler.columns.key.protoport_name,
+                   app.profiler.columns.value.avg_bytes,
+                   app.profiler.columns.value.network_rtt],
+        sort_col = app.profiler.columns.value.avg_bytes,
+        timefilter = TimeFilter.parse_range("last 15 m"),
+        trafficexpr = TrafficFilter("host 10/8")
+        )
+    
+    # Retrieve and print data
+    data = report.get_data()
+    printer = pprint.PrettyPrinter(2)
+    printer.pprint(data[:20])
+
+
+if __name__ == '__main__':
+    ProfilerApp(main).run()
