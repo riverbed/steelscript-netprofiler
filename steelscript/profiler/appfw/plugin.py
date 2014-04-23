de367e4328fba19148f188b64aa8b8c881dc9956
Mike Garabedian <mike.garabedian@riverbed.com>
2014-04-23 10:32:37 -0400

    Update Plugins to use package versions instead

--- steelscript/profiler/appfw/plugin.py
+++ steelscript/profiler/appfw/plugin.py
@@ -5,13 +5,15 @@
 #   https://github.com/riverbed/flyscript-portal/blob/master/LICENSE ("License").
 # This software is distributed "AS IS" as set forth in the License.
 
+import pkg_resources
+
 from steelscript.appfw.core.apps.plugins import Plugin
 
 
 class ProfilerPlugin(Plugin):
     title = 'Profiler Datasource Plugin'
     description = 'A Portal datasource plugin with example report'
-    version = '0.1'
+    version = pkg_resources.get_distribution('steelscript.netprofiler').version
     author = 'Riverbed Technology'
 
     enabled = True
