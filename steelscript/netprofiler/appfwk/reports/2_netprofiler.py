646b0551862dd8504aa8ce732b305fefeffbadb9
Mike Garabedian <mike.garabedian@riverbed.com>
2014-05-17 11:25:26 -0400

    Update headers, 2014

--- steelscript/netprofiler/appfwk/reports/2_netprofiler.py
+++ steelscript/netprofiler/appfwk/reports/2_netprofiler.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2013 Riverbed Technology, Inc.
+# Copyright (c) 2014 Riverbed Technology, Inc.
 #
-# This software is licensed under the terms and conditions of the
-# MIT License set forth at:
-#   https://github.com/riverbed/flyscript-portal/blob/master/LICENSE ("License").
-# This software is distributed "AS IS" as set forth in the License.
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
+
 
 from steelscript.appfwk.apps.report.models import Report
 import steelscript.appfwk.apps.report.modules.yui3 as yui3
