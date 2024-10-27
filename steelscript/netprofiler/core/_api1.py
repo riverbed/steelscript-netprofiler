a2f6a956a6baddc2ad9519944174b9ae4419bfc7
Gwen Blum <19484478+gwenblum@users.noreply.github.com>
2024-10-27 14:56:03 +0100

    Remove temporary triplet issue debugging

--- steelscript/netprofiler/core/_api1.py
+++ steelscript/netprofiler/core/_api1.py
@@ -271,6 +271,7 @@ class Templates(API1Group):
 
 class Handler(object):
     def __init__(self, profiler):
+        # TODO: use the latest APIs
         self.report = Report('/api/profiler/1.0/reporting', profiler)
         self.devices = Devices('/api/profiler/1.0/devices', profiler)
         self.common = Common('/api/common/1.0', profiler)
